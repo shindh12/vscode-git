b9b86a36a6177adcf121b2bd9c6fc821b74c662c
donghashin <shindh159@gmail.com>
2018-09-13 08:43:11 +0900

    초기 환경 설정

--- contact-app/src/main.js
+++ contact-app/src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import ES6Promise from 'es6-promise'
+ES6Promise.polyfill()
 
 Vue.config.productionTip = false
 
