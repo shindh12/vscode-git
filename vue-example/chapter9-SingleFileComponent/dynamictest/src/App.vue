5cb00fc136b43eb627f35e26d77cf86b857415bb
donghashin <shindh159@gmail.com>
2018-09-12 15:09:45 +0900

    동적 컴포넌트 예제

--- vue-example/chapter9-SingleFileComponent/dynamictest/src/App.vue
+++ vue-example/chapter9-SingleFileComponent/dynamictest/src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <div class="header">
+    <h1 class="headerText">(주) OpenSG</h1>
+    <nav>
+      <ul>
+        <li>
+            <!-- current view 값을 바꿔줌 -->
+          <a href="#" @click="changeMenu('home')">Home</a>
+        </li>
+        <li>
+          <a href="#" @click="changeMenu('about')">About</a>
+        </li>
+        <li>
+          <a href="#" @click="changeMenu('contact')">Contact</a>
+        </li>
+      </ul>
+    </nav>
   </div>
+  <!-- 여기 위치에 컴포넌트 이름에 따라 바뀜 -->
+  <div class="container">
+      <!-- 캐싱 콤마로 컴포넌트 여러개 사용 -->
+      <keep-alive include="home,about">
+          <component v-bind:is="currentView"></component>
+      </keep-alive>
+  </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
+// 기본은 참조한 이름의 소문자
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name : 'App',
+    components : { Home, About, Contact },
+    data : function () {
+        return { currentView : 'home' }
+    },
+    methods : {
+        changeMenu(view) {
+            this.currentView = view;
+        }
+    }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+.header { background-color:aqua; padding: 10px 0px 0px 0px; }
+.headerText { padding: 0px 20px 0px 20px; } 
+ul { list-style-type: none; margin: 0; padding: 0;
+    overflow: hidden; background-color: purple;  }
+li { float: left; }
+li a { display: block; color: yellow; text-align: center;
+    padding: 14px 16px; text-decoration: none;  }
+li a:hover { background-color: aqua; color:black; }
 </style>
