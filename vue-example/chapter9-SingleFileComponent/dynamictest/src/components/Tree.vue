b98506d9e02e004c02b3ce46c624e7c8490e6235
donghashin <shindh159@gmail.com>
2018-09-12 15:29:01 +0900

    재귀 컴포넌트 예제

---
+++ vue-example/chapter9-SingleFileComponent/dynamictest/src/components/Tree.vue
@@ -0,0 +1,19 @@
+<template>
+    <ul>
+        <li v-for="s in subs" :class="s.type" :key="s.name">
+            {{s.name}}
+            <tree :subs="s.subs"></tree>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name : "tree",
+    props : ["subs"]
+}
+</script>
+
+<style>
+
+</style>
