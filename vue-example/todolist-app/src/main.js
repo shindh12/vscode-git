515aa8cf017e30ad31b7715fb1df31873a754128
donghashin <shindh159@gmail.com>
2018-09-13 14:05:59 +0900

    store 인스턴스에 추가

--- vue-example/todolist-app/src/main.js
+++ vue-example/todolist-app/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import TodoList from './components/TodoList'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
+  store : store,
   render: h => h(TodoList)
 }).$mount('#app')
