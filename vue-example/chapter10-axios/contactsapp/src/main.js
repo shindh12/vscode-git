9a094837483771b9ce2868463985fce63471b2c7
donghashin <shindh159@gmail.com>
2018-09-13 10:21:44 +0900

    환경설정 변경

--- vue-example/chapter10-axios/contactsapp/src/main.js
+++ vue-example/chapter10-axios/contactsapp/src/main.js
@@ -1,10 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 // import App from './AppAxiosTest.vue'
-import axios from 'axios'
-Vue.config.productionTip = false
+import axios from 'axios';
+import ES6Promise from 'es6-promise';
+Vue.config.productionTip = false;
 Vue.prototype.$axios = axios;
+ES6Promise.polyfill();
 
 new Vue({
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
