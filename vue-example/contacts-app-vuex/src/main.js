9ca5f6aa63ec015174ef7112f7eb6dc8b58aa914
donghashin <shindh159@gmail.com>
2018-09-14 11:16:03 +0900

    모듈 변경

--- vue-example/contacts-app-vuex/src/main.js
+++ vue-example/contacts-app-vuex/src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue';
 import App from './App.vue';
 // import App from './AppAxiosTest.vue'
-import axios from 'axios';
+// import axios from 'axios';
+import store from './store'
 import ES6Promise from 'es6-promise';
+
 Vue.config.productionTip = false;
-Vue.prototype.$axios = axios;
+// Vue.prototype.$axios = axios;
 ES6Promise.polyfill();
 
 new Vue({
+  store,
   render: h => h(App)
 }).$mount('#app');
